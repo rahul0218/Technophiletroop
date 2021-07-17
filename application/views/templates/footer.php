80b9392c9f8e65b7f252fd11e050f5dc0afd5be4
Rahul Kumar <37036894+rahul0218@users.noreply.github.com>
2021-07-17 07:25:19 +0530

    first commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,129 @@
+
+
+    <!--  footer -->
+    <footr>
+      <div class="footer ">
+        <div class="container">
+          <div class="row">
+
+            <div class="col-md-12">
+              <form class="news">
+                <input class="newslatter" placeholder="Email" type="text" name=" Email">
+                <button class="submit">Subscribe</button>
+              </form>
+            </div>
+            <div class="col-md-12">
+              <h2>Newsletter</h2>
+              <span>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in  </span>
+            </div>
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 ">
+              <div class="row">
+                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 ">
+                  <div class="address">
+                    <h3>Contact us </h3>
+                    <ul class="loca">
+                      <li>
+                        <a href="#"><img src="assets/icon/loc.png" alt="#" /></a>London 145
+                        <br>United Kingdom </li>
+                        <li>
+                          <a href="#"><img src="assets/icon/email.png" alt="#" /></a>demo@gmail.com </li>
+                          <li>
+                            <a href="#"><img src="assets/icon/call.png" alt="#" /></a>+12586954775 </li>
+                          </ul>
+                          <ul class="social_link">
+                            <li><a href="#"><img src="assets/icon/fb.png"></a></li>
+                            <li><a href="#"><img src="assets/icon/tw.png"></a></li>
+                            <li><a href="#"><img src="assets/icon/lin(2).png"></a></li>
+                            <li><a href="#"><img src="assets/icon/instagram.png"></a></li>
+                          </ul>
+
+                        </div>
+                      </div>
+                      <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="address">
+                          <h3>Courses</h3>
+                          <ul class="Menu_footer">
+                            <li class="active"> <a href="#">Masters Degree</a> </li>
+                            <li><a href="#">Post GraduateU</a> </li>
+                            <li><a href="#">Ndergraduate</a> </li>
+                            <li><a href="#">Engineering</a> </li>
+                            <li><a href="#">Ph.D Degree</a> </li>
+                          </ul>
+                        </div>
+                      </div>
+                      <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="address">
+                          <h3>Information</h3>
+                          <ul class="Links_footer">
+                            <li class="active"><a href="#">Campus Tour</a> </li>
+                            <li><a href="#">Student Lifes</a> </li>
+                            <li><a href="#">Cholarship</a> </li>
+                            <li><a href="#"> Admission</a> </li>
+                            <li><a href="#">Leadership</a> </li>
+                          </ul>
+                        </div>
+                      </div>
+
+                      <div class="col-lg-3 col-md-6 col-sm-6 ">
+                        <div class="address">
+                          <a href="index.html"> <img src="assets/images/logo1.png" alt="logo"></a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+
+              </div>
+              <div class="copyright">
+                <div class="container">
+                  <p>Copyright © 2019 Design by <a href="https://html.design/">Free Html Templates </a></p>
+                </div>
+              </div>
+            </div>
+          </footr>
+          <!-- end footer -->
+          <!-- Javascript files-->
+          <script src="assets/js/jquery.min.js"></script>
+          <script src="assets/js/popper.min.js"></script>
+          <script src="assets/js/bootstrap.bundle.min.js"></script>
+          <script src="assets/js/jquery-3.0.0.min.js"></script>
+          <script src="assets/js/plugin.js"></script>
+          <!-- sidebar -->
+          <script src="assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+          <script src="assets/js/custom.js"></script>
+          <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+
+
+          <script>
+// This example adds a marker to indicate the position of Bondi Beach in Sydney,
+// Australia.
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: {
+      lat: 40.645037,
+      lng: -73.880224
+    },
+  });
+
+  var image = 'assets/images/maps-and-flags.png';
+  var beachMarker = new google.maps.Marker({
+    position: {
+      lat: 40.645037,
+      lng: -73.880224
+    },
+    map: map,
+    icon: image
+  });
+}
+</script>
+<!-- google map js -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8eaHt9Dh5H57Zh0xVTqxVdBFCvFMqFjQ&callback=initMap"></script>
+<!-- end google map js -->
+
+
+
+</body>
+
+</html>
